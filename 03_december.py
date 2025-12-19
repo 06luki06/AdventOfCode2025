ad4dcecb6aeed8b7cbace4a82c4605d1e3107d92
06luki06 <lukas.riegler2000@gmail.com>
2025-12-19 11:18:34 +0100

    FInish part two of 03 dec.

--- 03_december.py
+++ 03_december.py
@@ -1,32 +1,24 @@
 from functools import reduce
 
+# Monotonic Stack algorithm
+def get_max_joltage(numbers, target_len):
+    stack = []
+    to_remove = len(numbers) - target_len
+    
+    for num in numbers:
+        while to_remove > 0 and stack and stack[-1] < num:
+            stack.pop()
+            to_remove -= 1
+        stack.append(num)
+    
+    return int("".join(map(str, stack[:target_len])))
+
 with open('./inputs/03_input.txt', 'r') as file:
     lines = file.read().strip().split('\n')
 
-highest_numbers = []
-
+total_joltage = 0
 for line in lines:
-    numbers = [int(d) for d in str(line)]
-    lineDict = {}
-    for idx, number in enumerate(numbers):
-        lineDict[idx] = number
+    numbers = [int(d) for d in line.strip()]
+    total_joltage += get_max_joltage(numbers, 12)
 
-    max_key = max(lineDict, key=lineDict.get)
-    print("Max Key: {0} Value: {1}".format(max_key, lineDict[max_key]))
-    max_key_first_position = True
-
-
-    subset = {k: v for k, v in lineDict.items() if k > max_key}
-    if subset == {}:
-        subset = {k: v for k, v in lineDict.items() if k < max_key}
-        max_key_first_position = False
-
-    max_subset_key = max(subset, key=subset.get)
-    print("Subset Max Key: {0} Value: {1}".format(max_subset_key, subset[max_subset_key]))
-
-    if max_key_first_position:
-        highest_numbers.append("".join([str(lineDict[max_key]), str(subset[max_subset_key])]))
-    else:
-        highest_numbers.append("".join([str(subset[max_subset_key]), str(lineDict[max_key])]))
-
-print(reduce(lambda a, b: a + int(b), highest_numbers, 0))
+print(total_joltage)
